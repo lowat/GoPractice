7deec4a777d6b251a9924a17885f4b6976c7bd7b
Logan Watkins <lw2014wa@gmail.com>
2023-03-16 20:42:09 -0500

    Add tournamentWinner.go

---
+++ tournamentWinner.go
@@ -0,0 +1,28 @@
+package main
+
+const HOME_TEAM_WON = 1
+
+func TournamentWinner(competitions [][]string, results []int) string {
+	currentBestTeam := ""
+	scores := map[string]int{currentBestTeam: 0}
+
+	for idx, competition := range competitions {
+		result := results[idx]
+		homeTeam, awayTeam := competition[0], competition[1]
+		winningTeam := awayTeam
+		if result == HOME_TEAM_WON {
+			winningTeam = homeTeam
+		}
+		updateScores(winningTeam, 3, scores)
+
+		if scores[winningTeam] > scores[currentBestTeam] {
+			currentBestTeam = winningTeam
+		}
+	}
+
+	return currentBestTeam
+}
+
+func updateScores(team string, points int, scores map[string]int) {
+	scores[team] += points
+}
